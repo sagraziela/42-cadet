4efedbbd912d5e6e149a794debdc7b0f255e5076
Graziela De Souza Antunes <gde-souz@c3r3p4.42sp.org.br>
2023-09-05 17:41:27 -0300

    updated lib - grade 125 moulinette

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 13:01:10 by root              #+#    #+#             */
+/*   Updated: 2023/08/09 12:12:51 by gde-souz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
