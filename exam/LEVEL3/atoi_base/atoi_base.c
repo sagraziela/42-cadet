2a654763d71a824bff057750d4700ddea3404f5b
Graziela De Souza Antunes <gde-souz@c1r4p2.42sp.org.br>
2024-01-17 15:01:14 -0300

    print_hex + atoi_base

--- exam/LEVEL3/atoi_base/atoi_base.c
+++ exam/LEVEL3/atoi_base/atoi_base.c
@@ -6,12 +6,22 @@
 /*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:53:26 by root              #+#    #+#             */
-/*   Updated: 2024/01/12 11:15:09 by gde-souz         ###   ########.fr       */
+/*   Updated: 2024/01/17 13:26:52 by gde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 char	to_lower(char c)
 {
@@ -29,9 +39,17 @@ int	get_digit(char c, int base)
 	else
 		max_digit = base + 97;
 	if (c >= 48 && c <= 57)
+	{
+		if (c > max_digit)
+			get_digit(c - base, base);
 		return (c - 48);
+	}
 	else if (c >= 97 && c <= 102)
-		return (c + 10 - 'a');
+	{
+		if (c > max_digit)
+			get_digit(c - base, base);
+		return (c - 'a' + 10);
+	}
 	else
 		return (-1);
 }
@@ -39,76 +57,44 @@ int	get_digit(char c, int base)
 int	ft_atoi_base(const char *str, int str_base)
 {
 	int		i;
+	int		x;
+	int		factor;
 	int		sign;
 	int		digit;
 	int		result;
+	int		len;
 
 	i = 0;
+	x = 0;
 	sign = 1;
 	result = 0;
-	if (str[i] == '-')
-	{
+	len = ft_strlen((char *)str) - 1;
+	factor = 1;
+	if (str[0] == '-')
 		sign = -1;
-		i++;
-	}
-	while (str[i] != '\0')
+	while (str[len])
 	{
-		digit = get_digit(to_lower(str[i]), str_base);
-		result = (result * str_base) + digit;
-		i++;
+		digit = get_digit(to_lower(str[len]), str_base);
+		if (i > 0)
+		{
+			while (x < i)
+			{
+				factor *= 16;
+				x++;
+			}
+			i++;
+			x = 0;
+		}
+		else
+			i++;
+		result += (digit * factor);
+		len--;
 	}
 	return (result * sign);
 }
 
 int	main(void)
 {
-	printf("%d\n", ft_atoi_base("10", 16));
+	printf("%d\n", ft_atoi_base("2a", 2));
 	return (0);
 }
-
-// char	to_lower(char c)
-// {
-// 	if (c >= 65 && c <= 90)
-// 		return (c + 32);
-// 	return (c);
-// }
-
-// int	get_digit(char c, int base)
-// {
-// 	int	max_digit;
-
-// 	if (base <= 10)
-// 		max_digit = base + 48;
-// 	else
-// 		max_digit = base + 97;
-// 	if (c >= 48 && c <= 57)
-// 		return (c - 48);
-// 	else if (c >= 97 && c <= 102)
-// 		return (c + 10 - 'a');
-// 	else
-// 		return (-1);
-// }
-
-// int	ft_atoi_base(const char *str, int str_base)
-// {
-// 	int		i;
-// 	int		sign;
-// 	int		digit;
-// 	int		result;
-
-// 	i = 0;
-// 	sign = 1;
-// 	result = 0;
-// 	if (str[i] == '-')
-// 	{
-// 		sign = -1;
-// 		i++;
-// 	}
-// 	while (str[i] != '\0')
-// 	{
-// 		digit = get_digit(to_lower(str[i]), str_base);
-// 		result = (result * str_base) + digit;
-// 		i++;
-// 	}
-// 	return (result * sign);
-// }
--- exam/LEVEL3/print_hex/print_hex.c
+++ exam/LEVEL3/print_hex/print_hex.c
@@ -6,31 +6,57 @@
 /*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 16:43:47 by gde-souz          #+#    #+#             */
-/*   Updated: 2024/01/15 16:58:55 by gde-souz         ###   ########.fr       */
+/*   Updated: 2024/01/17 12:10:02 by gde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+
+int	ft_atoi(char *nbr)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (nbr[i] && (nbr[i] < 48 || nbr[i] > 57))
+	{
+		if (nbr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nbr[i] >= 48 && nbr[i] <= 57)
+	{
+		res = (res * 10) + (nbr[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
+
+void	dec_to_hex(int num)
+{
+	char	hex[16] = "0123456789abcdef";
+
+	if (num >= 16)
+	{
+		dec_to_hex(num / 16);
+	}
+	write(1, &hex[num % 16], 1);
+}
 
 int	main(int argc, char **argv)
 {
 	int		i;
 	int		num;
-	char	hex[16] = "0123456789abcdef";
 
 	i = 0;
 	if (argc == 2)
 	{
 		num = ft_atoi(argv[1]);
-		if (num >= 0 && num <= 16)
-			write(1, &hex[num], 1);
-		else
-		{
-			while (num)
-			{
-
-			}
-		}
+		dec_to_hex(num);
 	}
 	write(1, "\n", 1);
 	return (0);
