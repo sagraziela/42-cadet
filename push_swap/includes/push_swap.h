4c325bedbbe70a9eadf6b03453d902f508d2430a
root <root@DESKTOP-RV5CCQA>
2024-01-25 18:45:32 -0300

    add check_validity() function

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -3,28 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:45:27 by gde-souz          #+#    #+#             */
-/*   Updated: 2024/01/23 15:16:32 by gde-souz         ###   ########.fr       */
+/*   Updated: 2024/01/25 17:31:32 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# define RED     		"\033[1;31m"
+# define GREEN   		"\033[1;32m"
+# define YELLOW 		"\033[1;33m"
+# define BLUE   	 	"\033[1;34m"
+# define MAGENTA    	"\033[1;35m"
+# define CYAN    		"\033[1;36m"
+# define WHITE    		"\033[1;37m"
+# define DFT   			"\033[0m"
+
 # include <limits.h>
 # include "./libft.h"
 
-typedef struct s_tab {
-	int				nbr;
-	struct t_tab	*prev;
-	struct t_tab	*next;
-}	t_tab;
+typedef struct s_tab
+{
+	int value;
+	int index;
+	int pos;
+	int target_pos;
+	int cost_a;
+	int cost_b;
+	struct s_tab *prev;
+	struct s_tab *next;
+} t_tab;
 
 typedef struct s_stack {
 	t_tab	*a_list;
 	t_tab	*b_list;
+	size_t	length;
 	long	highest;
 	long	lowest;
 	long	max_a;
--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:23:59 by gde-souz          #+#    #+#             */
-/*   Updated: 2024/01/23 15:41:13 by gde-souz         ###   ########.fr       */
+/*   Updated: 2024/01/25 18:44:43 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,59 +16,83 @@ void	init_stacks(t_stack **stacks)
 {
 	(*stacks)->a_list = NULL;
 	(*stacks)->b_list = NULL;
+	(*stacks)->length = 0;
 	(*stacks)->highest = 0;
 	(*stacks)->lowest = 0;
 	(*stacks)->max_a = 0;
 	(*stacks)->max_b = 0;
 }
 
+int	*check_validity(char *list, t_stack **stacks)
+{
+	char	**nbr_list;
+	int		*nbr;
+	int		len;
+	int		n;
+	
+	len = 0;
+	nbr_list = ft_split(list, ' ');
+	while (nbr_list[len])
+		len++;
+	(*stacks)->length = len;
+	nbr = (int *)malloc(sizeof(int) * (len + 1));
+	if (!nbr)
+		return (NULL);
+	nbr[len--] = '\0';
+	while (nbr[len--])
+	{
+		n = ft_atoi(list[len]);
+		if (n > 2147483647 || n < -2147483648)
+		{
+			//free(nbr);
+			//free(list);
+			return (NULL);
+		}
+		nbr[len] = n;
+	}
+	
+}
+
 int	fill_stacks(char *list, t_stack **stacks)
 {
 	int		i;
 	long	n;
-	int		len;
-	char	**nbr_list;
+	t_tab	*new_node;
 	t_tab	*temp;
 
 	i = 0;
-	len = 0;
 	temp = NULL;
-	nbr_list = ft_split(list, ' ');
-	while (nbr_list[len])
-		len++;
 	while (nbr_list[i])
 	{
 		n = ft_atoi(nbr_list[i]);
 		if (n <= INT_MAX && n >= INT_MIN)
 		{
-			temp = (*stacks)->a_list;
-			(*stacks)->a_list = (t_tab *)malloc(sizeof(t_tab) * (len + 1));
-			if (!((*stacks)->a_list))
+			if ((*stacks)->a_list != NULL)
+				temp = (*stacks)->a_list;
+			else
+				temp = new_node;
+			new_node = (t_tab *)malloc(sizeof(t_tab) * ((*stacks)->length + 1));
+			if (!(new_node))
 				return (0);
-			(*stacks)->a_list->nbr = n;
-			temp->next = (*stacks)->a_list;
-			if ((*stacks)->highest == 0)
-				(*stacks)->highest = n;
-			else
-				(*stacks)->highest = n;
-			if ((*stacks)->lowest == 0)
-				(*stacks)->lowest = n;
-			else
-				(*stacks)->lowest = n;
-			ft_printf("nbr[i] = %d\n", (*stacks)->a_list);
+			new_node->value = n;
+			new_node->index = 8;
+			new_node->next = NULL;
+			temp = new_node;
+			temp->next = new_node;
+			ft_lstadd_back((*stacks)->a_list, new_node);
+			ft_printf("nbr[i] = %d\n", new_node);
 			i++;
 		}
 		else
 			return (0);
 	}
-	nbr[i] = '\0';
 	return (len);
 }
 
 int	main(int argc, char **argv)
 {
 	int		i;
-	int		length;
+	int		*nbr_list;
 	t_stack	*stacks;
 
 	i = 0;
@@ -79,9 +103,13 @@ int	main(int argc, char **argv)
 		if (!stacks)
 			return (0);
 		init_stacks(&stacks);
-		length = fill_stacks(argv[1], &stacks);
-		if (length == 0)
+		nbr_list = check_validity(argv[1]);
+		if (nbr_list)
+			fill_stacks(nbr_list, &stacks);
+		else
+			ft_printf("%sError\n", BLUE);
 	}
+	ft_printf("%sHELLO\n", BLUE);
 	return (0);
 }
 
