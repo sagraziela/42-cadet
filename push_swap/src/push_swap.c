c26fe200f53725477c847af07c8cc13e2f27ddc6
Graziela De Souza Antunes <gde-souz@c1r3p2.42sp.org.br>
2024-02-28 15:37:39 -0300

    find_beginning()

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:36:25 by gde-souz          #+#    #+#             */
-/*   Updated: 2024/02/28 14:09:53 by gde-souz         ###   ########.fr       */
+/*   Updated: 2024/02/28 15:33:04 by gde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,16 @@ void	push_to_b(t_stack **stack)
 	}
 }
 
-int	a_and_b_must_swap(t_tab *a_list, t_tab *b_list)
+void	find_beginning(t_stack **stack)
 {
-	if ((b_list->next)
-		&& (a_list->index > a_list->next->index)
-		&& (b_list->index < b_list->next->index))
-		return (1);
-	return (0);
+	while ((*stack)->a_list->index != 1)
+	{
+		if ((*stack)->length / 2 < (*stack)->a_list->index)
+			ra(&(*stack)->a_list, &(*stack)->moves);
+		else if ((*stack)->length / 2 > (*stack)->a_list->index)
+			rra(&(*stack)->a_list, &(*stack)->moves);
+	}
+	print_sorted_list((*stack)->a_list, (*stack)->moves);
 }
 
 void	sort(t_stack **stack)
@@ -78,22 +81,16 @@ void	sort(t_stack **stack)
 		while ((*stack)->b_list
 			&& (*stack)->b_list->target_pos != (*stack)->a_list->pos)
 		{
-			if (get_list_length((*stack)->a_list) / 2 > (*stack)->b_list->target_pos)
+			if (get_list_length((*stack)->a_list) / 2
+				> (*stack)->b_list->target_pos)
 				ra(&(*stack)->a_list, &(*stack)->moves);
-			else if (get_list_length((*stack)->a_list) / 2 < (*stack)->b_list->target_pos)
+			else if (get_list_length((*stack)->a_list) / 2
+				< (*stack)->b_list->target_pos)
 				rra(&(*stack)->a_list, &(*stack)->moves);
 		}
 		pa(&(*stack)->a_list, &(*stack)->b_list, &(*stack)->moves);
-		print_sorted_list((*stack)->a_list, (*stack)->moves);
+		//print_sorted_list((*stack)->a_list, (*stack)->moves);
 	}
-	while ((*stack)->a_list->index != 1)
-	{
-		if ((*stack)->length / 2 < (*stack)->a_list->index)
-			ra(&(*stack)->a_list, &(*stack)->moves);
-		else if ((*stack)->length / 2 > (*stack)->a_list->index)
-			rra(&(*stack)->a_list, &(*stack)->moves);
-	}
-	print_sorted_list((*stack)->a_list, (*stack)->moves);
 }
 
 void	push_swap(t_stack *stack)
@@ -114,5 +111,6 @@ void	push_swap(t_stack *stack)
 		print_sorted_list(stack->b_list, stack->moves);
 		ft_printf("\n");
 		sort(&stack);
+		find_beginning(&stack);
 	}
 }
