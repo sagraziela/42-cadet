3bda971aee39278c31011545363d916bed50aa49
root <root@DESKTOP-RV5CCQA>
2024-06-20 13:37:35 -0300

    starting timestamp

--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 06:58:14 by root              #+#    #+#             */
-/*   Updated: 2024/05/16 07:28:27 by root             ###   ########.fr       */
+/*   Updated: 2024/06/20 13:34:57 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,25 +29,33 @@ void    *my_func()
     return (NULL);
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-    t_list *list;
-    int     n_philo;
-    int     i;
+    // t_list *list;
+    // int     n_philo;
+    // int     i;
+    struct  timeval tval;
+    time_t  t;
+    struct  tm *info;
+    //char    buffer[64];
 
-    i = 1;
-    n_philo = atoi(argv[0]);
-    pthread_mutex_init(&mutex, NULL);
-    while (i <= n_philo)
-    {
-        new_node();
-        // CREATE LIST WITH THE NUMBERS OF PIHLOSOPHERS PASSED AS ARGUMENT
-        if (pthread_create(&list->philo, NULL, my_func, NULL) != 0)
-            return (1);
-        if (pthread_join(&list->philo, NULL) != 0)
-            return (2);
-        i++;
-    }
-    printf("Balance: %d\n", balance);
-    exit(0);
+    gettimeofday(&tval, NULL);
+    t = tval.tv_sec;
+    info = localtime(&t);
+    printf("%s", asctime(info));
+    // i = 1;
+    // n_philo = atoi(argv[0]);
+    // pthread_mutex_init(&mutex, NULL);
+    // while (i <= n_philo)
+    // {
+    //     new_node();
+    //     // CREATE LIST WITH THE NUMBERS OF PIHLOSOPHERS PASSED AS ARGUMENT
+    //     if (pthread_create(&list->philo, NULL, my_func, NULL) != 0)
+    //         return (1);
+    //     if (pthread_join(&list->philo, NULL) != 0)
+    //         return (2);
+    //     i++;
+    // }
+    // printf("Balance: %d\n", balance);
+    // exit(0);
 }
