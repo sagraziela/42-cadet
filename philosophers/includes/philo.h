a3c462750efe87cc46aa5399e5d41e81eac81626
root <root@DESKTOP-RV5CCQA>
2024-06-20 15:55:07 -0300

    1 philo eat, think and sleep

--- philosophers/includes/philo.h
+++ philosophers/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 06:59:16 by root              #+#    #+#             */
-/*   Updated: 2024/05/16 07:27:21 by root             ###   ########.fr       */
+/*   Updated: 2024/06/20 15:34:15 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,37 @@
 # include <string.h>
 # include <stdio.h>
 # include <sys/time.h>
+# include <time.h>
 # include <pthread.h>
 
-typedef struct s_list
-{
-    pthread_t       philo;
-    struct s_list   *next;
-}   t_list;
+// typedef struct s_philo
+// {
+// 	pthread_t		thread;
+// 	int				id;
+// 	int				eating;
+// 	int				meals_eaten;
+// 	size_t			last_meal;
+// 	size_t			time_to_die;
+// 	size_t			time_to_eat;
+// 	size_t			time_to_sleep;
+// 	size_t			start_time;
+// 	int				num_of_philos;
+// 	int				num_times_to_eat;
+// 	int				*dead;
+// 	pthread_mutex_t	*r_fork;
+// 	pthread_mutex_t	*l_fork;
+// 	pthread_mutex_t	*write_lock;
+// 	pthread_mutex_t	*dead_lock;
+// 	pthread_mutex_t	*meal_lock;
+// }					t_philo;
+
+// typedef struct s_program
+// {
+// 	int				dead_flag;
+// 	pthread_mutex_t	dead_lock;
+// 	pthread_mutex_t	meal_lock;
+// 	pthread_mutex_t	write_lock;
+// 	t_philo			*philos;
+// }					t_program;
 
 #endif
--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -6,24 +6,45 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 06:58:14 by root              #+#    #+#             */
-/*   Updated: 2024/06/20 13:34:57 by root             ###   ########.fr       */
+/*   Updated: 2024/06/20 15:53:30 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int	balance = 0;
 pthread_mutex_t mutex;
+
+void    get_current_time(void)
+{
+    struct  timeval tval;
+    time_t  t;
+    struct  tm *info;
+    char    buffer[64];
+    
+    gettimeofday(&tval, NULL);
+    //printf("%s", asctime(info));
+    t = tval.tv_sec;
+    info = localtime(&t);
+    strftime(buffer, sizeof(buffer), "%d/%m/%Y - %H:%M:%S\n", info);
+    printf("%s", buffer);
+}
 
 void    *my_func()
 {
     int i;
 
     i = 0;
-    while (i++ < 1000000)
+    while (i++ < 1)
     {
         pthread_mutex_lock(&mutex);
-        balance++;
+        printf("EAT - ");
+        get_current_time();
+        sleep(5);
+        printf("THINK - ");
+        get_current_time();
+        sleep(5);
+        printf("SLEEP\n");
+        get_current_time();
         pthread_mutex_unlock(&mutex);
     }
     return (NULL);
@@ -31,31 +52,21 @@ void    *my_func()
 
 int main(void)
 {
-    // t_list *list;
-    // int     n_philo;
-    // int     i;
-    struct  timeval tval;
-    time_t  t;
-    struct  tm *info;
-    //char    buffer[64];
+    pthread_t philo1;
+    int     i;
+    int     n_philo;
 
-    gettimeofday(&tval, NULL);
-    t = tval.tv_sec;
-    info = localtime(&t);
-    printf("%s", asctime(info));
-    // i = 1;
-    // n_philo = atoi(argv[0]);
-    // pthread_mutex_init(&mutex, NULL);
-    // while (i <= n_philo)
-    // {
-    //     new_node();
-    //     // CREATE LIST WITH THE NUMBERS OF PIHLOSOPHERS PASSED AS ARGUMENT
-    //     if (pthread_create(&list->philo, NULL, my_func, NULL) != 0)
-    //         return (1);
-    //     if (pthread_join(&list->philo, NULL) != 0)
-    //         return (2);
-    //     i++;
-    // }
-    // printf("Balance: %d\n", balance);
-    // exit(0);
+    i = 1;
+    n_philo = 1;
+    pthread_mutex_init(&mutex, NULL);
+    while (i <= n_philo)
+    {
+        // CREATE LIST WITH THE NUMBERS OF PIHLOSOPHERS PASSED AS ARGUMENT
+        if (pthread_create(&philo1, NULL, my_func, NULL) != 0)
+            return (1);
+        if (pthread_join(philo1, NULL) != 0)
+            return (2);
+        i++;
+    }
+    exit(0);
 }
