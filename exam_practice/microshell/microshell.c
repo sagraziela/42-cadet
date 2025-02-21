b40d075f14f552c9539364ca5d38a6af3baf5428
Graziela Antunes <graziela.atns@gmail.com>
2025-02-21 15:11:00 +0000

    init conditions on microshell

--- exam_practice/microshell/microshell.c
+++ exam_practice/microshell/microshell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdio.h>
 
 // https://github.com/TalhaAksoy/microshellExam/blob/main/main.c
 // https://github.com/shackbei/microshell-42/blob/main/microshell.c
@@ -19,7 +20,23 @@ int main(int argc, char *argv[], char *env[])
     {
         argv = &argv[i + 1]; //the new argv start after the ; or |
         i = 0;
+        // now we count until we have all the info to exec the next child:
         while (argv[1] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
             i++;
+        if (strcmp(argv[0], "cd" == 0))
+        {
+            printf("Caiu no CD\n");
+        }
+        else if (i != 0 && strcmp(argv[i], ";") == 0) //exec in stdout
+        {
+            printf("CAIU NO PONTO-VIRGULA\n");
+        }
+        else if (i != 0 && strcmp(argv[i], "|") == 0)
+        {
+            //pipe(fd);
+            printf("CAIU NO PIPE\n");
+        }
+        close(tmp_fd);
+        return (0);
     }
 }
